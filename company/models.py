d13ce1f9699b735478aef8125690913f64bdf300
Adhivp <adhivp910@gmail.com>
2024-05-13 14:01:25 +0530

    Created models

--- company/models.py
+++ company/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CompanyAuth(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+class JobOpening(models.Model):
+    job_title = models.CharField(max_length=100)
+    experience_needed = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    job_description = models.TextField()
+    last_date_for_application = models.DateField()
+
+    def __str__(self):
+        return self.user.username
