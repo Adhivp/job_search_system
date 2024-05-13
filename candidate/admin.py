37e83d9af29d934dc2b5e84356c3407095befd6d
Adhivp <adhivp910@gmail.com>
2024-05-13 14:34:19 +0530

    Registered models

--- candidate/admin.py
+++ candidate/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import CandidateAuth
 # Register your models here.
+
+admin.site.register(CandidateAuth)
--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CompanyAuth,JobOpening
 
-# Register your models here.
+admin.site.register(CompanyAuth)
+admin.site.register(JobOpening)
