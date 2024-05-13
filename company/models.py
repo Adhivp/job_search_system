15dcf65e00d39fdb5db1c0611e789c5b990b3238
Adhivp <adhivp910@gmail.com>
2024-05-13 17:58:32 +0530

    Models update

--- company/models.py
+++ company/models.py
@@ -7,11 +7,11 @@ class CompanyAuth(models.Model):
     bio = models.TextField()
 
 class JobOpening(models.Model):
+    jobid = models.AutoField(primary_key=True)
+    company = models.ForeignKey(CompanyAuth, on_delete=models.CASCADE,null=True)
     job_title = models.CharField(max_length=100)
     experience_needed = models.CharField(max_length=100)
     salary = models.DecimalField(max_digits=10, decimal_places=2)
     job_description = models.TextField()
     last_date_for_application = models.DateField()
 
-    def __str__(self):
-        return self.user.username
